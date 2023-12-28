abf4d267f14f1628f320a3c64ff26b69c8211f16
johnbarsigo <john.barsigo@student.moringaschool.com>
2023-12-29 00:10:49 +0300

    In progress

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.getElementById ( "submit" ).addEventListener ( "click", function ( event ){
+    event.preventDefault();
+
+    let todo = document.getElementById ( "new-task-description" ).value;
+  
+    let unordered_list = document.getElementById ( "tasks" );
+    let li = document.createElement ( "li" );
+    li.textContent = todo;
+    unordered_list.appendChild ( li );
+  });
+
+  
 });
+
+//Something of your choice! The main objective is to add a feature that allows the
+//user's input to affect the DOM
